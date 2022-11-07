bb0dc2c766badc6c698f461e0549c58947c060eb
JAY-DUE <jaimecardenasfuentes@gmail.com>
2022-11-06 21:14:45 -0500

    entrega 5

--- loopback/backendproyectoH/src/application.ts
+++ loopback/backendproyectoH/src/application.ts
@@ -9,6 +9,7 @@ import {RestApplication} from '@loopback/rest';
 import {ServiceMixin} from '@loopback/service-proxy';
 import path from 'path';
 import {MySequence} from './sequence';
+import { AuthenticationComponent } from '@loopback/authentication';
 
 export {ApplicationConfig};
 
@@ -40,5 +41,7 @@ export class Aap extends BootMixin(
         nested: true,
       },
     };
+    this.component(AuthenticationComponent);
+    
   }
 }
---
+++ loopback/backendproyectoH/src/estrategiasAdmin.ts/estrategiasAdmin.ts
@@ -0,0 +1,8 @@
+// import { authenticationstrategy } from "@loopback/authentication";
+
+// export class  estrategiaAdmin implements authenticationstrategy
+// {
+//     name:string = "admin";
+
+//     async  authe
+// }
